ad6f1eb74a84ef35316c06d5159ec39a28d7aaeb
dream-tree <marcinklimek@yahoo.com>
2018-12-25 22:19:50 +0100

    formatting code

--- src/CodeWars/HumanReadableTime.java
+++ src/CodeWars/HumanReadableTime.java
@@ -9,22 +9,29 @@ public class HumanReadableTime {
 
     public static String makeReadable1(int seconds) {
         if (seconds < 60) {     // a minute
-            return String.format("00:00:%02d", seconds);
+            return String.format(
+                    "00:00:%02d",
+                    seconds);
         }
         if (seconds < 3600) {    // an hour
             int minutes = seconds / 60;
             seconds = seconds % 60;
-            return String.format("00:%02d:%02d", minutes, seconds);
+            return String.format(
+                    "00:%02d:%02d",
+                    minutes, seconds);
         } else {
             int hours = seconds / 3600;
             int minutes = (seconds / 60 ) % 60;
             seconds = seconds % 60;
-            return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+            return String.format(
+                    "%02d:%02d:%02d",
+                    hours, minutes, seconds);
         }
     }
 
     public static String makeReadable2(int seconds) {
-        return String.format("%02d:%02d:%02d",
+        return String.format(
+                "%02d:%02d:%02d",
                 seconds / 3600, (seconds / 60) % 60, seconds % 60);
     }
 }
