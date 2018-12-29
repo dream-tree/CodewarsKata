4d3b1b9a718c12496ecf4cf5b0fba3c359d0016e
dream-tree <marcinklimek@yahoo.com>
2018-12-29 19:01:54 +0100

    refactoring unit test case

--- src/codewars/test/CountTheDigitTest.java
+++ src/codewars/test/CountTheDigitTest.java
@@ -8,16 +8,13 @@ import codewars.exercises.CountTheDigit;
 
 class CountTheDigitTest {
 
-    private static void testing(int actual, int expected) {
-        assertEquals(expected, actual);
-    }
     @Test
     public void test() {
-        testing(CountTheDigit.nbDig(10, 1), 4);
-        testing(CountTheDigit.nbDig(25, 1), 11);
-        testing(CountTheDigit.nbDig(5750, 0), 4700);
-        testing(CountTheDigit.nbDig(11011, 2), 9481);
-        testing(CountTheDigit.nbDig(12224, 8), 7733);
-        testing(CountTheDigit.nbDig(11549, 1), 11905);    
+        assertEquals(CountTheDigit.nbDig(10, 1), 4);
+        assertEquals(CountTheDigit.nbDig(25, 1), 11);
+        assertEquals(CountTheDigit.nbDig(5750, 0), 4700);
+        assertEquals(CountTheDigit.nbDig(11011, 2), 9481);
+        assertEquals(CountTheDigit.nbDig(12224, 8), 7733);
+        assertEquals(CountTheDigit.nbDig(11549, 1), 11905);    
     }
 }
